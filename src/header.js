8975be786be36ceb9e261c15e34cc59a0e3f787c
Luke <mackenzieluke94@gmail.com>
2019-03-04 11:13:29 -0500

    minor header change

--- src/header.js
+++ src/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <header>
-            <h1 className="headerTitle">Note-It!</h1>
+            <h1 className="headerTitle">Note-It</h1>
         </header>
     )
 }
